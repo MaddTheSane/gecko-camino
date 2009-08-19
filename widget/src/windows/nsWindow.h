a13a0f1bc8ee68b57f7885f313e88d90ad411a02
Felipe Gomes <felipc@gmail.com>
2009-08-18 23:20:53 -0700

    Bug 503541 - Fine grained control of gesture registration on widgets. r=smaug+roc, sr=smaug, approval1.9.2=beltzner

--- widget/src/windows/nsWindow.h
+++ widget/src/windows/nsWindow.h
@@ -415,6 +415,7 @@ protected:
   HKL                   mLastKeyboardLayout;
   nsPopupType           mPopupType;
   int                   mScrollSeriesCounter;
+  PRPackedBool          mDisplayPanFeedback;
 
   static PRUint32       sInstanceCount;
   static TriStateBool   sCanQuit;
