5b6107e1bf768c296368026aad6e694465491966
David Anderson <danderson@mozilla.com>
2009-08-24 17:09:44 -0700

    Removed JSStackFrame::callee (bug 512029, r=brendan).

--- js/src/jsinterp.cpp
+++ js/src/jsinterp.cpp
@@ -1293,7 +1293,6 @@ have_fun:
     frame.callobj = NULL;
     frame.argsobj = NULL;
     frame.script = script;
-    frame.callee = funobj;
     frame.fun = fun;
     frame.argc = argc;
     frame.argv = argv;
@@ -1525,7 +1524,6 @@ js_Execute(JSContext *cx, JSObject *chain, JSScript *script,
         frame.callobj = down->callobj;
         frame.argsobj = down->argsobj;
         frame.varobj = down->varobj;
-        frame.callee = down->callee;
         frame.fun = down->fun;
         frame.thisp = down->thisp;
         if (down->flags & JSFRAME_COMPUTED_THIS)
@@ -1544,7 +1542,6 @@ js_Execute(JSContext *cx, JSObject *chain, JSScript *script,
                 obj = tmp;
         }
         frame.varobj = obj;
-        frame.callee = NULL;
         frame.fun = NULL;
         frame.thisp = chain;
         frame.argc = 0;
