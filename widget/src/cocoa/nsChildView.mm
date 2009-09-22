29d8b1218b4668a36fe6d4653c69762129992c69
Markus Stange <mstange@themasta.com>
2009-09-22 14:42:07 +1200

    Bug 514648 - Non-resizable windows don't have resizers, even though -[NSWindow showsResizeIndicator] pretends they do. r=josh, a192=josh

--- widget/src/cocoa/nsChildView.mm
+++ widget/src/cocoa/nsChildView.mm
@@ -1197,7 +1197,8 @@ PRBool nsChildView::ShowsResizeIndicator(nsIntRect* aResizerRect)
   while ((superView = [topLevelView superview]))
     topLevelView = superView;
 
-  if (![[topLevelView window] showsResizeIndicator])
+  if (![[topLevelView window] showsResizeIndicator] ||
+      !([[topLevelView window] styleMask] & NSResizableWindowMask))
     return PR_FALSE;
 
   if (aResizerRect) {
