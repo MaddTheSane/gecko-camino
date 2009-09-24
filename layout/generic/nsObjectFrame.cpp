c2291116df6b659a8c169dc77e2b13a891aa743d
Josh Aas <joshmoz@gmail.com>
2009-09-24 17:03:50 -0400

    Stop sending bogus (incomplete) events to plugins on Mac OS X when the object frame is asked to handle events we don't care about. b=518135 r=roc

--- layout/generic/nsObjectFrame.cpp
+++ layout/generic/nsObjectFrame.cpp
@@ -4437,6 +4437,9 @@ nsEventStatus nsPluginInstanceOwner::ProcessEvent(const nsGUIEvent& anEvent)
         case NS_MOUSE_BUTTON_UP:
           carbonEvent.what = mouseUp;
           break;
+        default:
+          pluginWidget->EndDrawPlugin();
+          return nsEventStatus_eIgnore;
         }
       }
 
