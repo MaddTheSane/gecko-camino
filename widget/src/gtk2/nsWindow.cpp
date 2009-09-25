2ba642442b68273eaa81f8128c5286bcac8484cd
Mark Finkle <mark.finkle@gmail.com>
2009-09-25 02:14:19 -0400

    Bug 515761: URLBar textbox will auto capitalize on n900 [r=masayuki a=pavlov]

--- widget/src/gtk2/nsWindow.cpp
+++ widget/src/gtk2/nsWindow.cpp
@@ -6788,6 +6788,9 @@ nsWindow::SetIMEEnabled(PRUint32 aState)
             else if (mIMEData->mEnabled == nsIWidget::IME_STATUS_PASSWORD)
                mode |= HILDON_GTK_INPUT_MODE_INVISIBLE;
 
+            // Turn off auto-captitalization for editboxes
+            mode &= ~HILDON_GTK_INPUT_MODE_AUTOCAP;
+
             g_object_set (G_OBJECT(IMEGetContext()), "hildon-input-mode", (HildonGtkInputMode)mode, NULL);
             gIMEVirtualKeyboardOpened = PR_TRUE;
             hildon_gtk_im_context_show (IMEGetContext());
