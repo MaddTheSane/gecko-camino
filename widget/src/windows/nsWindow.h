c386afb18c9e56f853152fc8a1d3cb47341828e9
Kyle Huey <me@kylehuey.com>
2009-10-07 00:53:29 -0400

    Mouse wheel scrolling broken with Katmouse enabled - bug 514927 r+a=roc

--- widget/src/windows/nsWindow.h
+++ widget/src/windows/nsWindow.h
@@ -273,6 +273,9 @@ protected:
   virtual void            SubclassWindow(BOOL bState);
   void                    GetNonClientBounds(nsIntRect &aRect);
   PRBool                  CanTakeFocus();
+#if !defined(WINCE)
+  static void             InitTrackPointHack();
+#endif
 
   /**
    * Event processing helpers
@@ -440,6 +443,7 @@ protected:
   static PRBool         sJustGotDeactivate;
   static PRBool         sJustGotActivate;
   static int            sTrimOnMinimize;
+  static PRBool         sTrackPointHack;
 
   // Hook Data Memebers for Dropdowns. sProcessHook Tells the
   // hook methods whether they should be processing the hook
