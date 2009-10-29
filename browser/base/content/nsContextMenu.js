013dfd7fca2ab4450cc916395bda64d550b262b9
Dão Gottwald <dao@mozilla.com>
2009-10-29 08:42:45 +0100

    Bug 524106 - "View Image Info" selects the wrong image when there are multiple images with the same src. r=db48x

--- browser/base/content/nsContextMenu.js
+++ browser/base/content/nsContextMenu.js
@@ -238,7 +238,7 @@ nsContextMenu.prototype = {
     document.getElementById("context-viewbgimage")
             .disabled = !this.hasBGImage;
 
-    this.showItem("context-viewimageinfo", (this.onImage));
+    this.showItem("context-viewimageinfo", this.onImage);
   },
 
   initMiscItems: function CM_initMiscItems() {
@@ -754,7 +754,8 @@ nsContextMenu.prototype = {
   },
 
   viewImageInfo: function() {
-    BrowserPageInfo(this.target.ownerDocument.defaultView.top.document,"mediaTab",this.mediaURL);
+    BrowserPageInfo(this.target.ownerDocument.defaultView.top.document,
+                    "mediaTab", this.target);
   },
 
   viewFrameInfo: function() {
