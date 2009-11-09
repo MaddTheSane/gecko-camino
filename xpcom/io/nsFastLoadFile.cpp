0ac30159b70aafa2d2ec22cbe852972409ded74f
Taras Glek <tglek@mozilla.com>
2009-11-09 23:15:00 +0100

    Bug 524313 - crash [@ RaiseException ] nsFastLoadFileReader::ReadFooter r=brendan

--- xpcom/io/nsFastLoadFile.cpp
+++ xpcom/io/nsFastLoadFile.cpp
@@ -950,6 +950,15 @@ nsFastLoadFileReader::Open()
     if (NS_FAILED(rv))
         return rv;
 
+    PRUint32 checksum;
+    rv = ComputeChecksum(&checksum);
+    if (NS_FAILED(rv))
+        return rv;
+    
+    if (checksum != mHeader.mChecksum) {
+        return NS_ERROR_FAILURE;
+    }
+
     if (mHeader.mVersion != MFL_FILE_VERSION)
         return NS_ERROR_UNEXPECTED;
     if (mHeader.mFooterOffset == 0)
