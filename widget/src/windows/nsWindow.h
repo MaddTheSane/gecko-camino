837e6585bb99a846ff5465eb650203c0d272327a
Alex Pakhotin <alexp@mozilla.com>
2010-01-25 12:35:57 -0800

    Bug 532929 - Show SIP button and a toolbar on windows mobile. r=blassey a192=stuart

--- widget/src/windows/nsWindow.h
+++ widget/src/windows/nsWindow.h
@@ -493,7 +493,6 @@ protected:
 #endif
 
 #if defined(WINCE_HAVE_SOFTKB)
-  static PRBool         sSoftKeyMenuBar;
   static PRBool         sSoftKeyboardState;
 #endif // defined(WINCE_HAVE_SOFTKB)
 
