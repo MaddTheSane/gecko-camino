d00d71b2dce3e2dad06665af797001030bbd0687
Blake Kaplan <mrbkap@gmail.com>
2010-02-08 13:25:07 -0800

    Bug 544544 - Make sure DataContainerEvent's classinfo interfaces are complete. r=smaug (transplanted from 500068ba0693b443cf7e22a64e42b8f582b9ad2f)

--- dom/base/nsDOMClassInfo.cpp
+++ dom/base/nsDOMClassInfo.cpp
@@ -3598,6 +3598,7 @@ nsDOMClassInfo::Init()
 
   DOM_CLASSINFO_MAP_BEGIN(DataContainerEvent, nsIDOMDataContainerEvent)
     DOM_CLASSINFO_MAP_ENTRY(nsIDOMDataContainerEvent)
+    DOM_CLASSINFO_EVENT_MAP_ENTRIES
   DOM_CLASSINFO_MAP_END
 
   DOM_CLASSINFO_MAP_BEGIN(MessageEvent, nsIDOMMessageEvent)
