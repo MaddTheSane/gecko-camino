28ae5c318d601186c853ca5f8dc0a5f3820d3329
Karl Tomlinson <karlt+@karlt.net>
2010-03-24 16:37:06 +1300

    b=544211 provide synthesized button-up events when native button-up events are missed r=roc

--- widget/src/gtk2/nsWindow.h
+++ widget/src/gtk2/nsWindow.h
@@ -301,8 +301,8 @@ public:
     Window             mOldFocusWindow;
 #endif /* MOZ_X11 */
 
-    static guint32     mLastButtonPressTime;
-    static guint32     mLastButtonReleaseTime;
+    static guint32     sLastButtonPressTime;
+    static guint32     sLastButtonReleaseTime;
 
     NS_IMETHOD         BeginResizeDrag   (nsGUIEvent* aEvent, PRInt32 aHorizontal, PRInt32 aVertical);
 
@@ -564,6 +564,7 @@ private:
         *flag &= ~mask;
     }
 
+    void DispatchMissedButtonReleases(GdkEventCrossing *aGdkEvent);
 };
 
 class nsChildWindow : public nsWindow {
