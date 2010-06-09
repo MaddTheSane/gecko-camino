efd751be6ea74b983e5682e3dd10d9360d11318e
Jonathan Kew <jfkthame@gmail.com>
2010-06-09 08:12:19 +0100

    bug 559075 - add local autorelease pools to prevent Cocoa leaks during startup. r=josh a1.9.2.5=LegNeato

--- widget/src/cocoa/nsChildView.mm
+++ widget/src/cocoa/nsChildView.mm
@@ -572,6 +572,11 @@ nsresult nsChildView::Create(nsIWidget *aParent,
 {
   NS_OBJC_BEGIN_TRY_ABORT_BLOCK_NSRESULT;
 
+  // Because the hidden window is created outside of an event loop,
+  // we need to provide an autorelease pool to avoid leaking cocoa objects
+  // (see bug 559075).
+  nsAutoreleasePool localPool;
+
   // See NSView (MethodSwizzling) below.
   if (nsToolkit::OnLeopardOrLater() && !gChildViewMethodsSwizzled) {
     nsToolkit::SwizzleMethods([NSView class], @selector(mouseDownCanMoveWindow),
@@ -947,6 +952,11 @@ NS_IMETHODIMP nsChildView::Show(PRBool aState)
   NS_OBJC_BEGIN_TRY_ABORT_BLOCK_NSRESULT;
 
   if (aState != mVisible) {
+    // Provide an autorelease pool because this gets called during startup
+    // on the "hidden window", resulting in cocoa object leakage if there's
+    // no pool in place.
+    nsAutoreleasePool localPool;
+
     [mView setHidden:!aState];
     mVisible = aState;
     if (!mVisible)
@@ -6400,11 +6410,10 @@ nsTSMManager::CancelIME()
 OSStatus PluginKeyEventsHandler(EventHandlerCallRef inHandlerRef,
                                 EventRef inEvent, void *userData)
 {
-  id arp = [[NSAutoreleasePool alloc] init];
+  nsAutoreleasePool localPool;
 
   TSMDocumentID activeDoc = ::TSMGetActiveDocument();
   if (!activeDoc) {
-    [arp release];
     return eventNotHandledErr;
   }
 
@@ -6414,7 +6423,6 @@ OSStatus PluginKeyEventsHandler(EventHandlerCallRef inHandlerRef,
   if (status != noErr)
     target = nil;
   if (!target) {
-    [arp release];
     return eventNotHandledErr;
   }
 
@@ -6422,13 +6430,11 @@ OSStatus PluginKeyEventsHandler(EventHandlerCallRef inHandlerRef,
   status = ::GetEventParameter(inEvent, kEventParamTextInputSendKeyboardEvent,
                                typeEventRef, NULL, sizeof(EventRef), NULL, &keyEvent);
   if ((status != noErr) || !keyEvent) {
-    [arp release];
     return eventNotHandledErr;
   }
 
   [target processPluginKeyEvent:keyEvent];
 
-  [arp release];
   return noErr;
 }
 
