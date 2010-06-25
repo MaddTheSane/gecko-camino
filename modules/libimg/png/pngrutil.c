315da5be1f3b208bb64774665a17cde1eb39cbdb
Glenn Randers-Pehrson <glennrp+bmo@gmail.com>
2010-06-25 17:07:48 -0400

    Bug 492200 - Upgrade libpng to 1.2.37; r=joe sr=vlad a1.9.2.6=dveditz

--- modules/libimg/png/pngrutil.c
+++ modules/libimg/png/pngrutil.c
@@ -3377,6 +3377,8 @@ defined(PNG_USER_TRANSFORM_PTR_SUPPORTED)
    {
      png_free(png_ptr, png_ptr->big_row_buf);
      png_ptr->big_row_buf = (png_bytep)png_malloc(png_ptr, row_bytes+64);
+     if (png_ptr->interlaced)
+       png_memset(png_ptr->big_row_buf, 0, row_bytes + 64);
      png_ptr->row_buf = png_ptr->big_row_buf+32;
      png_ptr->old_big_row_buf_size = row_bytes+64;
    }
