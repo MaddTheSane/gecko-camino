2017d0b9a15ccfb63afcc8bb7e0c7f21cb9e65ce
Boris Zbarsky <bzbarsky@mit.edu>
2010-10-15 17:55:53 -0400

    Bug 602780.  Fix handling of hosts in NS_SecurityCompareURIs.  r=jst, a=dveditz

--- netwerk/base/public/nsNetUtil.h
+++ netwerk/base/public/nsNetUtil.h
@@ -50,6 +50,7 @@
 
 #include "nsCRT.h"
 #include "nsIURI.h"
+#include "nsIStandardURL.h"
 #include "nsIInputStream.h"
 #include "nsIOutputStream.h"
 #include "nsISafeOutputStream.h"
@@ -1530,7 +1531,7 @@ NS_SecurityHashURI(nsIURI* aURI)
 
     nsCAutoString host;
     PRUint32 hostHash = 0;
-    if (NS_SUCCEEDED(baseURI->GetHost(host)))
+    if (NS_SUCCEEDED(baseURI->GetAsciiHost(host)))
         hostHash = nsCRT::HashCode(host.get());
 
     // XOR to combine hash values
@@ -1621,6 +1622,13 @@ NS_SecurityCompareURIs(nsIURI* aSourceURI,
     nsCAutoString sourceHost;
     if (NS_FAILED( targetBaseURI->GetAsciiHost(targetHost) ) ||
         NS_FAILED( sourceBaseURI->GetAsciiHost(sourceHost) ))
+    {
+        return PR_FALSE;
+    }
+
+    nsCOMPtr<nsIStandardURL> targetURL(do_QueryInterface(targetBaseURI));
+    nsCOMPtr<nsIStandardURL> sourceURL(do_QueryInterface(sourceBaseURI));
+    if (!targetURL || !sourceURL)
     {
         return PR_FALSE;
     }
