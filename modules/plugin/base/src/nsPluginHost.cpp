25e5d79ad58add4e95cca4779f4bb3840e788096
Scott Greenlay <devnull@localhost>
2011-01-12 00:32:42 -0800

    Bug 478823: Fix memory leak that can result from reloading the plugin list. r=josh r=jst a1.9.2.14=bsterne

--- modules/plugin/base/src/nsPluginHost.cpp
+++ modules/plugin/base/src/nsPluginHost.cpp
@@ -4728,21 +4728,13 @@ nsresult nsPluginHost::ScanPluginsDirectory(nsIFile * pluginsDir,
           *aPluginsChanged = PR_TRUE;
         }
       }
-    }
-    else {
-      // plugin file was added, flag this fact
-      *aPluginsChanged = PR_TRUE;
-    }
-
-    // if we are not creating the list, just continue the loop
-    // no need to proceed if changes are detected
-    if (!aCreatePluginList) {
-      if (*aPluginsChanged)
-        return NS_OK;
-      else
+      if (!aCreatePluginList) {
+        if (*aPluginsChanged)
+          return NS_OK;
         continue;
+      }
     }
-
+    
     // if it is not found in cache info list or has been changed, create a new one
     if (!pluginTag) {
       nsPluginFile pluginFile(file);
@@ -4835,6 +4827,14 @@ nsresult nsPluginHost::ScanPluginsDirectory(nsIFile * pluginsDir,
 
     // do it if we still want it
     if (bAddIt) {
+      // We have a valid new plugin so report that plugins have changed.
+      *aPluginsChanged = PR_TRUE;
+      
+      // If we're not creating a plugin list, simply looking for changes,
+      // then we're done.
+      if (!aCreatePluginList) {
+        return NS_OK;
+      }
       pluginTag->SetHost(this);
       pluginTag->mNext = mPlugins;
       mPlugins = pluginTag;
